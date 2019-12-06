795d212e66a0b29415e03c9f37ba302091ac34be
hc <909697030@qq.com>
2019-12-06 22:30:51 +0800

    2019-12-6转到github同步到br mac

--- src/App.vue
+++ src/App.vue
@@ -9,18 +9,6 @@
       <router-link class="nav-items" to="/bing">必应美图</router-link>
       <router-link class="nav-items" to="/daily">每日英语</router-link>
       <router-link class="nav-items" to="/individual">个人中心</router-link>
-
-      <div class="nav-profile borderBlue">
-        <img src="./assets/profile/default_profile1.png">
-        <div class="nav-profile-detail">
-          <ul class="profile-detail">
-            <li class="profile-userName">{{  }}</li>
-            <li class="profile-userRank">等级：{{}}</li>
-            <li class="profile-userShare">分享：{{}}</li>
-          </ul>
-          <div class="profile-logout">退出登陆</div>
-        </div>
-      </div>
     </div>
     <router-view class="routerView"></router-view>
   </div>
@@ -28,4 +16,4 @@
 
 
 <style src="@/assets/css/app.css"></style>
-<script scoped src="@/assets/js/app.js"></script>
+<script src="@/assets/js/app.js"></script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,31 @@
 <template>
-    <center>
-        <p>主页</p>
-    </center>
+    <div class='container'>
+        <!-- 背景层 -->
+        <div class="bg-cover">
+            <img class="bg-image" src="../assets/image/500628783.jpg">
+            <div class="mask"></div>
+        </div>
+        <!-- 标题层 -->
+        <div class="title-cover">
+            <div class="title-head">美好生活 &nbsp; 从壁纸开始</div>
+            <div class="title-commit">成立于2019年11月<br>
+                TuTu社区致力于成为亚洲最大的图片分享网站<br></div>
+        </div>
+
+        <!-- 脚注层 -->
+        <div class="footer-cover">
+            <span class="link">设为首页 </span> 
+            <span> ©2019 TuTu </span> 
+            <span class="link"> 使用图图前必读 </span> 
+            <span> 京ICP证000001号 </span>
+            <span>
+                <a class="link"
+                    href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=10000000000000">京公网安备11000002000001号</a>
+            </span>
+        </div>
+
+    </div>
 </template>
+
+<style scoped src="@/assets/css/home.css"></style>
+<script scoped src="@/assets/js/home.js"></script>
