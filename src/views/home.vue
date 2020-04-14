5a19534b81c261160ac6f0429586681061bc4abc
hec <chun.he@100credit.com>
2020-04-14 19:38:32 +0800

    修改首页搜索框样式

--- src/views/home.vue
+++ src/views/home.vue
@@ -12,7 +12,7 @@
                 />
                 <div class="search-btn">
                     <span @click="search">
-                        <svg focusable="false" fill="#4285f4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                        <svg focusable="false" fill="#5caef1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
                             <path
                                 d="M15.5 14h-.79l-.28-.27A6.471 6.471 0 0 0 16 9.5 6.5 6.5 0 1 0 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"
                             ></path>
@@ -77,16 +77,17 @@ export default {
 .search.search-input {
     width: 100%;
     height: inherit;
-    color: #000;
     font-size: 18px;
     font-weight: 400;
     letter-spacing: 0.1em;
     padding: 7px 55px 7px 25px;
     border-radius: 25px;
+    background: #abf3;
+    box-shadow: 0 0 3 3 #fff;
 }
 
 .search-input:focus {
-    color: steelblue;
+    color: #5caef1;
 }
 
 .search-btn {
@@ -98,5 +99,15 @@ export default {
     right: 20px;
     margin: auto;
     cursor: pointer;
+}
+
+input::-webkit-input-placeholder {
+    color: #abf7;
+}
+input::-moz-input-placeholder {
+    color: #abf7;
+}
+input::-ms-input-placeholder {
+    color: #abf7;
 }
 </style>
