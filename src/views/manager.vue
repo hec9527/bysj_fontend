a4d523bfaf858ce3210ecb5215bb949d620345b0
hec <chun.he@100credit.com>
2020-04-17 18:18:34 +0800

    新增分享页面和后台管理页面

---
+++ src/views/manager.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="container">
+        <p></p>
+        <p></p>
+        <br />
+        <br />
+        <center>管理员登录才能看到</center>
+        <p>不是管理员手动跳转到该页面自动跳转到首页</p>
+    </div>
+</template>
+
+<script>
+export default {
+    data: () => {
+        return {
+            //
+        };
+    }
+};
+</script>
+
+<style scoped>
+/*  */
+</style>
---
+++ src/views/share.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="container">
+        <br />
+        <br />
+        <center>用户分享</center>
+        <p>只有登录之后的用户才能进入</p>
+    </div>
+</template>
+
+<script>
+export default {
+    data: () => {
+        return {
+            //
+        };
+    }
+    // 未登录的用户进入直接跳转到首页
+};
+</script>
+
+<style scoped>
+/*  */
+</style>
