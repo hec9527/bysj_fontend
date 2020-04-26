8dd7acc20bdc858dafdf90d778acda3dfbc5a35a
hec <chun.he@100credit.com>
2020-04-26 17:49:30 +0800

    假数据填充 页面VU界面

--- src/component/manager-uv.vue
+++ src/component/manager-uv.vue
@@ -1,23 +1,39 @@
 <template>
     <el-table :data="tableData">
-        <el-table-column prop="date" label="日期" width="140" />
-        <el-table-column prop="vu" label="VU数据" width="160" />
-        <el-table-column prop="address" label="地址" />
+        <el-table-column prop="date" label="日期" />
+        <el-table-column prop="path" label="页面路由" />
+        <el-table-column prop="vu" label="VU数据" />
+        <el-table-column prop="click" label="点击数" />
     </el-table>
 </template>
 
 <script>
 export default {
     data: () => {
-        return {
-            tableData: [
-                {
-                    date: new Date().toLocaleDateString(),
-                    vu: `${(Math.random() * 3000) | 0}/${(Math.random() * 2000) | 0}`,
-                    address: '家里蹲'
+        const ONE_DAY = 86400000;
+        const TODAY = new Date().getTime();
+        const path = ['/home', '/category', '/biying', '/daily', '/share', '/indi'];
+        const getDate = () => {
+            let data = [];
+            for (let i = 0; i < 10; i++) {
+                for (let j = 0, len = path.length; j < len; j++) {
+                    data.push({
+                        date: new Date(TODAY - ONE_DAY * i).toLocaleDateString(),
+                        vu: `${(Math.random() * 3000) | 0}/${(Math.random() * 2000) | 0}`,
+                        path: path[j],
+                        click: ((Math.random() * 2000) | 0) + 200
+                    });
                 }
-            ]
+            }
+            return data;
         };
+
+        return {
+            tableData: getDate()
+        };
+    },
+    mounted() {
+        console.log(this.tableData);
     }
 };
 </script>
