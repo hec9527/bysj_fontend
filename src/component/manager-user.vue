0ae2dc2f097c5f7cdcf81acf3d338b6cdad84144
hec <chun.he@100credit.com>
2020-04-27 16:51:08 +0800

    增加修改用户信息页面

--- src/component/manager-user.vue
+++ src/component/manager-user.vue
@@ -7,10 +7,10 @@
             </div>
             <div class="page-btns">
                 <div class="btn">
-                    <el-button type="info">查找</el-button>
+                    <el-button type="info" @click="searchUser">查找</el-button>
                 </div>
                 <div class="btn">
-                    <el-button type="danger">删除</el-button>
+                    <el-button type="danger" @click="searchUser">删除</el-button>
                 </div>
                 <div class="btn">
                     <el-button type="primary" @click="addUserInfo">新增</el-button>
@@ -22,14 +22,14 @@
         <div class="page-body">
             <el-table ref="userTable" :data="userDate" highlight-current-row style="width: 100%">
                 <el-table-column prop="id" label="用户ID" fixed />
-                <el-table-column prop="count" label="账户" fixed width="120" />
+                <el-table-column prop="count" label="账户" fixed width="160" />
                 <el-table-column prop="avator" label="头像">
                     <template slot-scope="scope">
                         <img v-if="scope.row.avator" :src="scope.row.avator" />
                         <i v-if="!scope.row.avator" class="el-icon-user-solid"></i>
                     </template>
                 </el-table-column>
-                <el-table-column prop="name" label="昵称" width="200" />
+                <el-table-column prop="name" label="用户名" width="200" />
                 <el-table-column prop="age" label="年龄" />
                 <el-table-column prop="sex" label="性别">
                     <template slot-scope="scope">
@@ -38,20 +38,24 @@
                 </el-table-column>
                 <el-table-column prop="vip" label="VIP" />
                 <el-table-column prop="level" label="等级" />
-                <el-table-column prop="balance" label="账户" width="100" />
+                <el-table-column prop="balance" label="余额" width="140" />
                 <el-table-column prop="permission" label="权限">
                     <template slot-scope="scope">
                         {{ scope.row.permission === 0 ? '管理员' : '普通用户' }}
                     </template>
                 </el-table-column>
                 <el-table-column prop="qq" label="QQ" width="120" />
-                <el-table-column prop="wechat" label="微信" width="120" />
-                <el-table-column prop="phone" label="电话" width="120" />
-                <el-table-column prop="email" label="邮箱" width="140" />
-                <el-table-column prop="addr" label="地址" width="200" />
-                <el-table-column prop="motto" label="签名" width="250" />
+                <el-table-column prop="wechat" label="微信" width="160" />
+                <el-table-column prop="phone" label="电话" width="140" />
+                <el-table-column prop="email" label="邮箱" width="200" />
+                <el-table-column prop="addr" label="地址" width="250" />
+                <el-table-column prop="motto" label="签名" width="400" />
                 <el-table-column prop="lasttime" label="最后登录" width="100" />
-                <el-table-column prop="forbidden" label="禁用" />
+                <el-table-column prop="forbidden" label="禁用">
+                    <template slot-scope="scope">
+                        {{ scope.row.forbidden === 0 ? '已禁用' : '已启用' }}
+                    </template>
+                </el-table-column>
                 <el-table-column fixed="right" label="操作" width="100">
                     <template slot-scope="scope">
                         <el-button @click="deleteUser(scope.row.id)" type="text" size="small">删除</el-button>
@@ -61,8 +65,8 @@
             </el-table>
         </div>
 
-        <!-- 页面modal窗口 -->
-        <el-dialog :title="isEdit ? '编辑用户信息' : '新增用户信息'" width="460" :visible.sync="showModal">
+        <!-- 添加新用户 -->
+        <el-dialog title="新增用户信息" width="460" :visible.sync="addUserInfoModal">
             <el-form ref="form" :model="form" label-width="80px">
                 <el-form-item label="账户名">
                     <el-input v-model="form.count"></el-input>
@@ -81,10 +85,71 @@
                     </el-select>
                 </el-form-item>
             </el-form>
-
             <span slot="footer" class="dialog-footer">
-                <el-button @click="showModal = false">取 消</el-button>
+                <el-button @click="addUserInfoModal = false">取 消</el-button>
                 <el-button type="primary" @click="saveUserInfo">确 定</el-button>
+            </span>
+        </el-dialog>
+
+        <!-- 修改已有用户 -->
+        <el-dialog title="修改用户信息" width="460" :visible.sync="editUserInfoModal">
+            <el-form ref="form" :model="editData" label-width="80px">
+                <el-form-item label="账户名">
+                    <el-input v-model="editData.count"></el-input>
+                </el-form-item>
+                <el-form-item label="用户名">
+                    <el-input v-model="editData.name"></el-input>
+                </el-form-item>
+                <el-form-item label="密码">
+                    <el-input v-model="editData.passwd" type="password"></el-input>
+                </el-form-item>
+                <el-form-item label="年龄">
+                    <el-input v-model="editData.age" type="number" min="0" max="200"></el-input>
+                </el-form-item>
+                <el-form-item label="性别">
+                    <el-select v-model="editData.sex" de placeholder="来选一个吧">
+                        <el-option label="男" value="0"></el-option>
+                        <el-option label="女" value="1"></el-option>
+                        <el-option label="保密" value="2"></el-option>
+                    </el-select>
+                </el-form-item>
+                <el-form-item label="VIP">
+                    <el-input v-model="editData.vip" type="number" min="0" max="6"></el-input>
+                </el-form-item>
+                <el-form-item label="等级">
+                    <el-input v-model="editData.level" type="number" min="0" max="6"></el-input>
+                </el-form-item>
+                <el-form-item label="余额">
+                    <el-input v-model="editData.balance" type="number" min="0" max="9999999"></el-input>
+                </el-form-item>
+                <el-form-item label="权限">
+                    <el-input v-model="editData.permission" type="number" min="0" max="1"></el-input>
+                </el-form-item>
+                <el-form-item label="QQ">
+                    <el-input v-model="editData.qq"></el-input>
+                </el-form-item>
+                <el-form-item label="微信">
+                    <el-input v-model="editData.wechat"></el-input>
+                </el-form-item>
+                <el-form-item label="电话">
+                    <el-input v-model="editData.phone"></el-input>
+                </el-form-item>
+                <el-form-item label="邮箱">
+                    <el-input v-model="editData.email"></el-input>
+                </el-form-item>
+                <el-form-item label="地址">
+                    <el-input v-model="editData.addr"></el-input>
+                </el-form-item>
+                <el-form-item label="签名">
+                    <el-input v-model="editData.motto"></el-input>
+                </el-form-item>
+                <el-form-item label="禁用">
+                    <el-input v-model="editData.forbidden" type="number" max="1" min="0"></el-input>
+                </el-form-item>
+            </el-form>
+            <span slot="footer" class="dialog-footer">
+                <el-button @click="editUserInfoModal = false">取 消</el-button>
+                <el-button type="primary" @click="saveEditUserInfo">确 定</el-button>
             </span>
         </el-dialog>
     </div>
@@ -100,8 +165,8 @@ export default {
     data: () => {
         return {
             count: 0,
-            showModal: false,
-            isEdit: false,
+            addUserInfoModal: false,
+            editUserInfoModal: false,
             editData: {},
             form: {}
         };
@@ -121,11 +186,11 @@ export default {
             );
         },
         editUser(info) {
-            console.log(info);
+            this.editData = info;
+            this.editUserInfoModal = true;
         },
         addUserInfo() {
-            this.showModal = !this.showModal;
-            this.isEdit = false;
+            this.addUserInfoModal = !this.addUserInfoModal;
         },
         saveUserInfo() {
             const checkResult = checklogin(this.form.count, this.form.passwd);
@@ -134,7 +199,7 @@ export default {
             }
             const loadingInstance = Loading.service({ fullscreen: true, text: 'loading' });
             setTimeout(() => {
-                this.showModal = false;
+                this.addUserInfoModal = false;
                 loadingInstance.close();
             }, 30 * 1000);
             API.addUserInfo(this.form)
@@ -146,8 +211,27 @@ export default {
                     () => console.error('用户添加失败')
                 )
                 .finally(() => {
-                    this.showModal = false;
+                    this.addUserInfoModal = false;
                     loadingInstance.close();
+                });
+        },
+        saveEditUserInfo() {
+            const checkResult = checklogin(this.editData.count, this.editData.passwd);
+            if (!checkResult.flag) {
+                return Notification.error(checkResult.message);
+            }
+            const loadingInstance = Loading.service({ fullscreen: true, text: 'loading' });
+            API.updateUserInfo(this.editData)
+                .then(
+                    res => {
+                        Notification.service(res.mes);
+                        this.fetchUserList();
+                    },
+                    () => console.log(`修改用户数据错误：${this.editData}`)
+                )
+                .finally(() => {
+                    loadingInstance.close();
+                    this.editUserInfoModal = false;
                 });
         },
         fetchUserList() {
@@ -155,6 +239,9 @@ export default {
                 res => this.$store.commit('UPDATE_USER_LIST', res.data),
                 () => console.warn('所有用户信息获取失败')
             );
+        },
+        searchUser() {
+            Notification.warning('别点了，这里还没完成');
         },
         ...mapGetters(['getAllUserInfo'])
     },
